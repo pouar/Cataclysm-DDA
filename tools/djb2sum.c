2e39eb3e72c5023b0931d170eb2333b15e2c85e6
Pouar <thepouar@gmail.com>
2014-10-29 15:02:31 -0500

    use dynamic char array so we won't run out of room

--- tools/djb2sum.c
+++ tools/djb2sum.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int djb2_hash(unsigned char *str)
 {
@@ -15,10 +16,29 @@ int djb2_hash(unsigned char *str)
 
 int main(int argc, char *argv[])
 {
-	char djb2_char[4096];
-	read(0,djb2_char, 4096 );
+	unsigned int len_max = 1024;
+	unsigned int current_size = 0;
+	char *djb2_char = malloc(len_max);
+	current_size = len_max;
+	if(djb2_char != NULL)
+	{
+		char c = EOF;
+		unsigned int i =0;
+		while (( c = getchar() ) != EOF)
+		{
+			djb2_char[i++]=(char)c;
+			if(i == current_size)
+			{
+				current_size = i+len_max;
+				djb2_char = realloc(djb2_char, current_size);
+			}
+		}
+		djb2_char[i] = '\0';
+	}
 	printf("%d",(signed int)djb2_hash((unsigned char *)djb2_char));
 	if (!*++argv || strcmp(*argv, "-n"))
 		putchar('\n');
+	free(djb2_char);
+	djb2_char = NULL;
 	return 0;
 }
